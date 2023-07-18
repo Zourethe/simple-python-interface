32e2d12842a1445091da82508a7bec2728f7c0bb
Zourethe <pedresilva4@gmail.com>
2023-07-18 11:10:29 -0300

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-                            Simple Python Interface
+                                                    Simple Python Interface
     
     This is the main file of the script, the one that will load the main frame of
     the interface.
@@ -9,29 +9,28 @@
 '''
 
 # Libraries imports.
-import tkinter
-import tkinter.font
+from tkinter import Tk, font, Frame, Button, Label
 
 # Variables definition.
 text = str('Press the button above')
 
 # Root variable definition.
-root = tkinter.Tk()
-frame = tkinter.Frame(root, height = 500, width = 500)
+root = Tk()
+frame = Frame(root, height = 500, width = 500)
 frame.pack()
 
 # Font definition.
-font = tkinter.font.Font(family = "Arial", size = 24)
+font = font.Font(family = "Arial", size = 24)
 
 # Execute method definition.
 def execute():
     print("Output")
 
 # Button definition.
-button = tkinter.Button(root, text = "Press me", font = font, command = execute, fg = '#0000FF')
+button = Button(root, text = "Press me", font = font, command = execute, fg = '#0000FF')
 
 # Label definition.
-label = tkinter.Label(root, text = text, font = font, fg = '#FF0000')
+label = Label(root, text = text, font = font, fg = '#FF0000')
 
 # Packs definition.
 button.pack(padx = 0, pady = 10, side = tkinter.TOP)
