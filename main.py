976fd5f36a40d78008cc0b5adac52c7f0d1ba576
Zourethe <pedresilva4@gmail.com>
2023-07-18 18:35:59 -0300

    Updated

--- main.py
+++ main.py
@@ -9,28 +9,29 @@
 '''
 
 # Libraries imports.
-from tkinter import Tk, font, Frame, Button, Label
+import tkinter
+import tkinter.font
 
 # Variables definition.
 text = str('Press the button above')
 
 # Root variable definition.
-root = Tk()
-frame = Frame(root, height = 500, width = 500)
+root = tkinter.Tk()
+frame = tkinter.Frame(root, height = 500, width = 500)
 frame.pack()
 
 # Font definition.
-font = font.Font(family = "Arial", size = 24)
+font = tkinter.font.Font(family = "Arial", size = 24)
 
 # Execute method definition.
 def execute():
     print("Output")
 
 # Button definition.
-button = Button(root, text = "Press me", font = font, command = execute, fg = '#0000FF')
+button = tkinter.Button(root, text = "Press me", font = font, command = execute, fg = '#0000FF')
 
 # Label definition.
-label = Label(root, text = text, font = font, fg = '#FF0000')
+label = tkinter.Label(root, text = text, font = font, fg = '#FF0000')
 
 # Packs definition.
 button.pack(padx = 0, pady = 10, side = tkinter.TOP)
